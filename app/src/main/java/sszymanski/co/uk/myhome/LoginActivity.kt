f8b007f13e22fc181a057ae06dfbcee477897892
Sylwester Szymanski <sylwester.moniuszko-szymanski@yotta.co.uk>
2018-01-28 23:20:39 +0000

    implemented login front-end validation

--- app/src/main/java/sszymanski/co/uk/myhome/LoginActivity.kt
+++ app/src/main/java/sszymanski/co/uk/myhome/LoginActivity.kt
@@ -2,11 +2,26 @@ package sszymanski.co.uk.myhome
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import sszymanski.co.uk.myhome.utils.showToast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonLogIn.setOnClickListener({
+            val login = editTextLogin.text.toString()
+            val password = editTextPassword.text.toString()
+            if (validateCredentials(login, password)) // TODO send credentials to server
+            else it.showToast(getString(R.string.log_in_password_or_login_wrong))
+
+        })
     }
+
+    /**
+     * Validates if provided fields are not empty and password is at least 6 letters
+     */
+    fun validateCredentials(login: String, password: String) = !login.isEmpty() && password.length >= 6;
+
 }
--- app/src/main/java/sszymanski/co/uk/myhome/utils/Utils.kt
+++ app/src/main/java/sszymanski/co/uk/myhome/utils/Utils.kt
@@ -1,8 +1,13 @@
 package sszymanski.co.uk.myhome.utils
 
+import org.apache.commons.codec.digest.DigestUtils
+
 /**
  * Created by rex on 28/01/2018.
  */
 class Utils {
 
+    companion object {
+        fun getMd5Hash(password:String) = DigestUtils.md5Hex(password);
+    }
 }
