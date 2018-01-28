118e6b624fb96399cdf54e1d154aa3b558eb1715
Sylwester Szymanski <sylwester.moniuszko-szymanski@yotta.co.uk>
2018-01-28 23:20:06 +0000

    added Kotlin extenstions with Toast View extenstion

---
+++ app/src/main/java/sszymanski/co/uk/myhome/utils/KotlinExtensions.kt
@@ -0,0 +1,16 @@
+package sszymanski.co.uk.myhome.utils
+
+import android.view.View
+import android.widget.Toast
+
+/**
+ * Created by rex on 28/01/2018.
+ */
+
+
+/**
+ * Extenstion to view for easy showing Toasts. Default lenght is short, can be modified by passing second parameter
+ */
+fun View.showToast(message:String, length:Int = Toast.LENGTH_SHORT){
+    Toast.makeText(context, message, length).show();
+}
