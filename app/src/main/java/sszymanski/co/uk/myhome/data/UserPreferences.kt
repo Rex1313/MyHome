958142587239b6b6dff6feac83a901012bbeedd6
Sylwester Szymanski <sylwester.moniuszko-szymanski@yotta.co.uk>
2018-01-29 00:15:04 +0000

    Added Shared Preferences to store login data ( comments added )

--- app/src/main/java/sszymanski/co/uk/myhome/data/UserPreferences.kt
+++ app/src/main/java/sszymanski/co/uk/myhome/data/UserPreferences.kt
@@ -11,11 +11,20 @@ class UserPreferences(val context: Context) {
     val USER_LOGIN = "USER_LOGIN"
     val USER_PASSWORD = "USER_PASSWORD"
 
+    /**
+     * Saves Login details in shared preferences name and md5 hashed password
+     * @nameAndPassword: Pair of name and md5 hash of password, where first element is name, and second element is password
+     */
     fun saveLoginDetails(nameAndPassword: Pair<String, String>) {
         val sharedPreferences = context.getSharedPreferences(USER_SHARED_PREFERNCES, Context.MODE_PRIVATE)
         sharedPreferences.save(USER_LOGIN, nameAndPassword.first)
         sharedPreferences.save(USER_PASSWORD, nameAndPassword.second)
     }
+
+    /**
+     * Loads Login details from shared preferences
+     * @return name and md5 hash of password in Pair, where first element it name and second element is password
+     */
     fun loadLoginDetails(): Pair<String, String>{
         val sharedPreferences = context.getSharedPreferences(USER_SHARED_PREFERNCES, Context.MODE_PRIVATE)
         val nameAndPassword = Pair(sharedPreferences.getString(USER_LOGIN, ""), sharedPreferences.getString(USER_PASSWORD, ""))
