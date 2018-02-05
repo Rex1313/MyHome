f313c37e11aaa173914355cfafef7577d274fd0d
Sylwester Moniuszko-Szymanski <sylwester.moniuszko-szymanski@yotta.co.uk>
2018-02-05 22:56:48 +0000

    Code cleaning for Login

---
+++ app/src/test/java/sszymanski/co/uk/myhome/LoginUnitTest.kt
@@ -0,0 +1,27 @@
+package sszymanski.co.uk.myhome
+
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.mock
+import sszymanski.co.uk.myhome.login.LoginActivity
+import sszymanski.co.uk.myhome.login.LoginMvp
+import sszymanski.co.uk.myhome.login.LoginPresenter
+
+/**
+ * Created by rex on 04/02/2018.
+ */
+class LoginUnitTest {
+    lateinit var mockedView:LoginActivity
+    lateinit var presenter: LoginMvp.Presenter;
+    @Before
+    fun initialize() {
+        mockedView = mock(LoginActivity::class.java)
+        presenter = LoginPresenter(mockedView)
+    }
+
+    @Test
+    fun test_login_success(){
+        presenter.onLoginSuccess()
+    }
+
+}
